e94717190f0f124bfe6d0706e288f498582d14ca
Zain Mehmood <106885156+zainwen9@users.noreply.github.com>
2024-02-16 18:41:07 +0500

    initial commit

---
+++ android/app/src/main/kotlin/com/example/ballon/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.ballon
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ballon.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class Ballon{
+  double x;
+  double y;
+  final Color color;
+  final int windDirection;
+
+  Ballon(
+      {required this.x,
+      required this.y,
+      required this.color,
+      required this.windDirection});
+}
